073a72750996a650e23fdc2025d5972bbb53264f
1234sen <105305316+1234sen@users.noreply.github.com>
2025-02-16 19:54:04 -0500

    Add files via upload
    updated

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 //Define data
 let data = [
-    { name: 'Rainne'    , rating: 8 },
-    { name: 'Buddy'    , rating: 7 },
-    { name: 'Paddy'   , rating: 3 },
-    { name: 'Sticky', rating: 9 },
-    { name: 'Midnight'  , rating: 5 },
-    { name: 'Leo'  , rating: 6 }
-  ];
+  { name: 'Rainne', rating: 8 },
+  { name: 'Buddy', rating: 7 },
+  { name: 'Paddy', rating: 3 },
+  { name: 'Sticky', rating: 9 },
+  { name: 'Midnight', rating: 5 },
+  { name: 'Leo', rating: 6 }
+];
 
-  let 
-  width = 600,
+let width = 600,
   height = 400;
 
 let margin = {
@@ -17,52 +16,53 @@ let margin = {
   bottom: 50,
   left: 50,
   right: 50
-}
+};
 
 let svg = d3.select('body')
-            .append('svg')
-            .attr('width', width)
-            .attr('height', height)
-            .style('background', 'lightyellow')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .style('background', 'lightblue'); // 修改背景颜色
 
 let yscale = d3.scaleLinear()
-              .domain([0,10])
-              .range([height - margin.bottom, margin.top])
+  .domain([0, 10])
+  .range([height - margin.bottom, margin.top]);
 
 let xscale = d3.scaleBand()
-              .domain(data.map(d => d.name))
-              .range([margin.left, width - margin.right])   
-              .padding(0.5)           
+  .domain(data.map(d => d.name))
+  .range([margin.left, width - margin.right])
+  .padding(0.5);
 
 let yaxis = svg.append('g')
-              .call(d3.axisLeft().scale(yscale))
-              .attr('transform', `translate(${margin.left} , 0)`)
-              
-let xaxis = svg.append('g')
-              .call(d3.axisBottom().scale(xscale))
-              .attr('transform', `translate(0,${height - margin.bottom})`)
+  .call(d3.axisLeft().scale(yscale))
+  .attr('transform', `translate(${margin.left}, 0)`);
 
-//Draw the labels
+let xaxis = svg.append('g')
+  .call(d3.axisBottom().scale(xscale))
+  .attr('transform', `translate(0,${height - margin.bottom})`);
+
+// Draw the labels
 svg.append('text')
   .text('Name')
-  .attr('x', width/2)
-  .attr('y', height - 15)
+  .attr('x', width / 2)
+  .attr('y', height - 15);
 
 svg.append('text')
   .text('Rating')
-  .attr('x', 0-height/2)
+  .attr('x', 0 - height / 2)
   .attr('y', 25)
-  .attr('transform', 'rotate(-90)')
+  .attr('transform', 'rotate(-90)');
 
 let line = d3.line()
-            .x(d => xscale(d.name) +xscale.bandwidth()/2)
-            .y(d => yscale(d.rating))
-            .curve(d3.curveNatural)
+  .x(d => xscale(d.name) + xscale.bandwidth() / 2)
+  .y(d => yscale(d.rating))
+  .curve(d3.curveNatural);
 
 let path = svg.append('path')
-              .datum(data)
-              .attr('d', line)
-              .attr('fill', 'none')
-              .attr('stroke', 'black')
-              .attr('stroke-width', 2)
+  .datum(data)
+  .attr('d', line)
+  .attr('fill', 'none')
+  .attr('stroke', 'orange') // 修改 stroke 颜色
+  .attr('stroke-width', 2);
+
 
